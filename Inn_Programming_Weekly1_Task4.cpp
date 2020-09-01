faa3afefa3bd61ee0678cfc02c6a4e61e2eb5d63
ganffor <69429261+ganffor@users.noreply.github.com>
2020-09-01 20:20:55 +0200

    Add project files.

---
+++ Inn_Programming_Weekly1_Task4.cpp
@@ -0,0 +1,23 @@
+//Task 2
+
+#include <iostream>
+
+char drink;
+
+int main()
+{
+	std::cout << "Whats's your favorite drink?" << std::endl << "1. Coffee" << std::endl <<
+		"2. Tea" << std::endl << "3. Coca Cola" << std::endl;
+	std::cin >> drink;
+	switch (drink)
+	{
+	case '1':
+		std::cout << "Coffee is delicious";
+		break;
+	case '2':
+		std::cout << "Tea gives peace of mind";
+		break;
+	case '3':
+		std::cout << "Coke will give you a white smile";
+	}
+}
